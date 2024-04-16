88671bb1b4f7cddbdaf3c11b4fbe4b51f79161cb
mmssl <muharremssl.001@gmail.com>
2024-04-16 17:11:14 +0300

    first commit

--- parent_child/test.h
+++ parent_child/test.h
@@ -2,13 +2,14 @@
 #define TEST_H
 
 #include <QObject>
+#include <QDebug>
 
 class Test : public QObject
 {
     Q_OBJECT
 public:
     explicit Test(QObject *parent = nullptr);
-
+    ~Test();
 signals:
 };
 
